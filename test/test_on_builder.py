c1c0f893b573be827997f1eb3a96d75e21b1d97a
giovanni-angeli <giovanni.angeli6@gmail.com>
2018-11-25 18:27:03 +0100

    re-organize test scripts

--- test/test_on_builder.py
+++ test/test_on_builder.py
@@ -1,6 +1,11 @@
 # coding: utf-8
 
-from .common import check_version
+from .common import check_version, check_with_virtual_ports
 
-def test():    
+def test_ver():    
+
     check_version()
+
+def test_comm():    
+
+    check_with_virtual_ports(time_to_live=2)
--- test/test_on_host.py
+++ test/test_on_host.py
@@ -1,20 +1,2 @@
 # coding: utf-8
 
-import os
-import sys
-import time
-import logging
-import traceback
-import asyncio
-
-import serial_dispatcher
-
-def test():
-
-    msg = "serial_dispatcher.__version__:{}".format(serial_dispatcher.__version__)
-    print (msg)
-
-
-if __name__ == '__main__':
-
-    test()
